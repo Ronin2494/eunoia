719020e590af19f25112206da1a04433bc55d48a
chiragvij63 <chiragvij63@gmail.com>
2020-10-22 21:25:04 -0400

    Patch-2-home.xml

--- app/src/main/java/com/example/eunoia/signin.java
+++ app/src/main/java/com/example/eunoia/signin.java
@@ -2,14 +2,26 @@ package com.example.eunoia;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class signin extends AppCompatActivity {
 
     TextView signup;
+    TextView email,password;
+    Button login;
+    FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +29,55 @@ public class signin extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         signup = findViewById(R.id.signup);
+        email = findViewById(R.id.login_email);
+        password = findViewById(R.id.login_password);
+        login = findViewById(R.id.loginButton);
+
+        fAuth = FirebaseAuth.getInstance();
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email1 = email.getText().toString().trim();
+                String password1 = password.getText().toString().trim();
+
+                //To check if user already existed or not
+                if(fAuth.getCurrentUser() != null){
+                    startActivity(new Intent(getApplicationContext(), home.class));
+                    finish();
+                }
+
+                if(TextUtils.isEmpty(email1)){
+                    email.setError("Email is required");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password1)){
+                    password.setError("Password is required");
+                    return;
+                }
+
+                if(password1.length() < 6){
+                    password.setError("password must be greater than 6 characters");
+                    return;
+                }
+
+                fAuth.signInWithEmailAndPassword(email1, password1).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(signin.this, "Login Successfully!", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), home.class));
+
+                        }else{
+                            Toast.makeText(signin.this, "Error!" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
+
 
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
