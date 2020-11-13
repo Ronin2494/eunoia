61b34d52a6407282e4f1151ec7f02bcd53eb938c
aniket3030 <aniketpatel3030@gmail.com>
2020-11-13 01:35:52 -0500

    New activity Activity_routinesleep

---
+++ app/src/main/java/com/example/eunoia/Activity_routinesleep.java
@@ -0,0 +1,14 @@
+package com.example.eunoia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_routinesleep extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_routinesleep);
+    }
+}
