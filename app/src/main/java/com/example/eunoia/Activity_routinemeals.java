769bf842e1625cd63d5a851a8ac83d9eed5f839f
aniket3030 <aniketpatel3030@gmail.com>
2020-11-13 02:16:03 -0500

    New activity Activity_routinemeals

--- app/src/main/java/com/example/eunoia/Activity_routinemeals.java
+++ app/src/main/java/com/example/eunoia/Activity_routinemeals.java
@@ -7,21 +7,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class Activity_routinesleep extends AppCompatActivity {
+public class Activity_routinemeals extends AppCompatActivity {
 
     Button btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_routinesleep);
+        setContentView(R.layout.activity_routinemeals);
 
-        btn1 = findViewById(R.id.button_submit);
+        btn1 = findViewById(R.id.btnRoutine);
 
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), Activity_routinemeals.class));
+                startActivity(new Intent(getApplicationContext(), Activity_routinesleep.class));
                 finish();
             }
         });
